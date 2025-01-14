a750e5ea7f728be4d2d6834742b5a5b32536868c
sjMun09 <ohoh7391@naver.com>
2025-01-14 15:16:59 +0900

    feat:도전 기능 가이드 lv 1to2 구현

---
+++ src/com/example/kiosk/Cart.java
@@ -0,0 +1,53 @@
+package com.example.kiosk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<MenuItem> items;
+    private CartStatus cartStatus;
+
+    private void updateCartStatus() {
+        if (items.isEmpty()) cartStatus = CartStatus.EMPTY;
+        else if (items.size() > 0) cartStatus = CartStatus.PARTIALLY_FILLED;
+        else cartStatus = CartStatus.FULL;
+    }
+
+    public Cart() {
+        this.items = new ArrayList<>();
+        updateCartStatus();
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+        updateCartStatus();
+    }
+
+    public void displayCart() {
+        if (cartStatus == CartStatus.EMPTY) {
+            System.out.println("장바구니가 비어 있습니다.");
+            return;
+        }
+        for (MenuItem item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public double getTotalPrice() {
+        return items.stream().mapToDouble(MenuItem::getPrice).sum();
+    }
+
+    public void clear() {
+        items.clear(); // 비워주고
+        updateCartStatus();
+    }
+
+    public boolean isEmpty() {
+        return cartStatus == CartStatus.EMPTY;
+    }
+
+    public CartStatus getCartStatus() {
+        return cartStatus;
+    }
+
+}
---
+++ src/com/example/kiosk/CartStatus.java
@@ -0,0 +1,8 @@
+package com.example.kiosk;
+
+/**
+ * cart 상태 구현
+ */
+public enum CartStatus {
+    EMPTY, PARTIALLY_FILLED, FULL
+}
